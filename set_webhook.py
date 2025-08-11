69d7b5ebc0986176e38c8dd787e17829caf470d6
Nicolas Daniel de Souza Bastos <nicolasdaniel16y@gmail.com>
2025-08-11 12:52:50 -0300

    Add files via upload

--- set_webhook.py
+++ set_webhook.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 TOKEN = os.getenv("TOKEN", "SEU_TOKEN_AQUI")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://seu-bot.onrender.com")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://telegram-bot-3ao7.onrender.com")
 
 def set_webhook():
     url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}"
